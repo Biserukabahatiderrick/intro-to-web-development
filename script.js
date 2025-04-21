7d52d9e193e968b24a5bf2a09c38fe2be791715b
BISERUKA BAHATI DERRICK <biserukabahatiderrick@gmail.com>
2025-04-21 00:08:45 +0000

    web-development

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function changeBackgroundColor(color){
     let section=document.getElementById('coding-journey');
-    section. style.changeBackgroundColor=color;
+    section. style.backgroundColor=color;
 }
-document.getElementById('intro').addEventListener(click,()=>changeBackgroundColor("#2CA02C"))
-document.getElementById('react').addEventListener(click,()=>changeBackgroundColor("#FF7F0E"))
-document.getElementById('backend').addEventListener(click,()=>changeBackgroundColor("#9467BD"))
+document.getElementById('intro').addEventListener('click',()=>changeBackgroundColor("#2CA02C"));
+document.getElementById('react').addEventListener('click',()=>changeBackgroundColor("#FF7F0E"));
+document.getElementById('backend').addEventListener('click',()=>changeBackgroundColor("#9467BD"));
 
+const year=new Date().getFullYear();
+const message=`Website Made With ❤️ From Kigali © ${year}`;
+document.getElementById('footer').textContent=message;
